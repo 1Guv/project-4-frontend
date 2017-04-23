fc36ae516784370d9a7bf9229476d3cc606c5a2d
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-04-23 13:43:06 +0100

    added new nav bar which is responsive as well using a toggle function in main controller

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -47,4 +47,18 @@ function MainCtrl($rootScope, $state, $auth) {
   }
 
   vm.logout = logout;
+
+
+  // Used to toggle the menu on small screens when clicking on the menu button
+  function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+  vm.toggleFunction = toggleFunction;
+
 }
